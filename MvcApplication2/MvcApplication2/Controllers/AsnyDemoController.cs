38af2e65aa8372e8b3d2ef9a7f3e9cde296ea3bc
Vincent Yang <mscsharp@gmail.com>
2012-09-03 12:49:52 +1000

    Add Mvc Async demo

---
+++ MvcApplication2/MvcApplication2/Controllers/AsnyDemoController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcApplication2.Controllers
+{
+    public class AsnyDemoController : AsyncController
+    {
+        //
+        // GET: /AsnyDemo/
+
+        public void IndexAsync()
+        {
+            AsyncManager.OutstandingOperations.Increment();
+            NewsService service = new NewsService();
+            var result = service.GetNews();
+            AsyncManager.Parameters["values"] = result;
+            AsyncManager.OutstandingOperations.Decrement();         
+        }
+
+        public ActionResult IndexCompleted(IEnumerable<News> values)
+        {            
+            return View(values);
+        }
+
+    }
+}
---
+++ MvcApplication2/MvcApplication2/NewsService.cs
@@ -0,0 +1,30 @@
+﻿using FizzWare.NBuilder;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Web;
+
+namespace MvcApplication2
+{
+    public class NewsService
+    {
+        public IEnumerable<News> GetNews()
+        {
+            Thread.Sleep(5000);
+            var result = Builder<News>.CreateListOfSize(50).Build();
+
+            return result;
+
+        }
+    }
+
+
+    public class News
+    {
+        public string Title { get; set; }
+        public int ID { get; set; }
+        public string ShortDescription { get; set; }
+    }
+
+}
